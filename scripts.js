344a6c9c6c5257d90752ad1165dc57c7aa8264cf
ZAHRAA-C146 <70736050+ZAHRAA-C146@users.noreply.github.com>
2020-10-10 21:02:44 +0300

    DONE

--- scripts.js
+++ scripts.js
@@ -1,12 +1,35 @@
 // 1
 // moon.jpg قم بالوصول إلى الصورة وتغيرها إلى
 
+ document.getElementById("image").src="./images/moon.jpg";  
+      
+
 // 2
 // "قم بالوصول إلى كلمة الأرض وتحويلها إلى "القمر
+ document.getElementById("caption");
+
+MOON.innerHTML="القمر";
 
 // 3
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
+function DARK () {
+    let MOON = document.getElementById("caption")
+    if(MOON.innerHTML=="القمر" ){
+       MOON.innerHTML ="الشمس"
+       let image=document.getElementById("image");
+       image.src="./images/sun.jpg"
+       let body =document.getElementById("body");
+       body.style.background="#bb3817"
+}
+else{
+    MOON.innerHTML="القمر";
+    let image=document.getElementById("image")
+    image.src="./images/moon.jpg"
+    let body = document.getElementById("body")
+    body.style.background="#ffffff"
+}
+}
 
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
